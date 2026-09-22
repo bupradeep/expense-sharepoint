ca42a02b650e02f48ea5d52b39185e421c3f59f9
Pradeep <bu.pradeep@gmail.com>
2026-09-21 20:15:04 -0700

    Initial commit before SPFx Heft migration

---
+++ src/webparts/expense/ExpenseWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'ExpenseWebPartStrings';
+import Expense from './components/Expense';
+import { IExpenseProps } from './components/IExpenseProps';
+
+export interface IExpenseWebPartProps {
+  description: string;
+}
+
+export default class ExpenseWebPart extends BaseClientSideWebPart<IExpenseWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IExpenseProps> = React.createElement(
+      Expense,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
